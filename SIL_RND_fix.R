6e427dbbbeecbd4eea4b3579ccd2b0bf09bab3b1
YIKIM Yjoan <59438814+cejuicreamice@users.noreply.github.com>
2020-07-13 08:15:49 +0900

    Update SIL_RND_fix.R
    duplicated overflow (35000 * ...) debug v0.15

--- SIL_RND_fix.R
+++ SIL_RND_fix.R
@@ -13,7 +13,10 @@ ap <- NULL
 ### R 3.6.1 x64의 경우 !duplicated 코드가 i가 35000이 넘어가면 에러가 납니다.
 ### 따라서 35000까지의 duplicated 제거 부분에 각 5000씩을 더해서 duplicated 제거를 한 거를 저장하고
 ### 누적해서 더하는 방식을 취해야 하는 것 같습니다.
+xxx2 <- xxx[!duplicated(xxx), ]
 
-xxx2 <- xxx[(!duplicated), ]
 ### 이거를 분리하는 방법이 필요합니다.
-st2_sep <- statuslist[(loop_i - big_inc) : (loop_i - 1)]
+st2_sep <- st_list[(loop_i - big_inc) : (loop_i - 1)]
+zzz2_t <- do.call("rbind", st2_sep)
+zzz2_b <- rbind(zzz2_b, zzz2_t)
+zzz2_b  <- zzz2_b[!
